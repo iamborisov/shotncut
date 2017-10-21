ed5c0f7c0b06fa11d66c9c3e3bc70671a1e47ff4
iamborisov <iamborisov@gmail.com>
2017-10-21 03:49:57 +0100

    Mailing refactoring

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Service\ContentService;
+use AppBundle\Service\MailerService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,147 +13,59 @@ class ContactController extends Controller
 {
     /**
      * @Route("/contact/", name="contact")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        return $this->render('site/contact.html.twig', [
-
-        ]);
+        return $this->render('site/contact.html.twig');
     }
 
     /**
      * @Route("/contact/send", name="contact_send")
+     *
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function sendAction(
-        Request $request,
-        \Swift_Mailer $mailer,
-        ContentService $settings
-    ) {
+    public function sendAction(Request $request) {
+        /** @var MailerService $mailer */
+        $mailer = $this->get('mailer.contact');
+
+        /** @var array $data */
         $data = $request->get('contact');
 
-        if (
-            !isset($data['name']) ||
-            !isset($data['email']) ||
-            !isset($data['text'])
-        ) {
+        if (!$mailer->validate($data)) {
             throw new BadRequestHttpException();
         }
 
-        $result = $this->sendEmail(
-            $mailer,
-            $settings,
-            $data['email'],
-            'contact',
-            $data
-        );
-
         return new JsonResponse([
-            'result' => $result ? 'ok' : 'error'
+            'result' => $mailer->send($data['email'], $data)
+                ? 'ok'
+                : 'error'
         ]);
     }
 
     /**
      * @Route("/contact/calc", name="contact_calc")
+     *
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function calcAction(
-        Request $request,
-        \Swift_Mailer $mailer,
-        ContentService $settings
-    ) {
+    public function calcAction(Request $request) {
+        /** @var MailerService $mailer */
+        $mailer = $this->get('mailer.calc');
+
+        /** @var array $data */
         $data = $request->get('calc');
 
-        if (
-            !isset($data['date']) ||
-            !isset($data['email']) ||
-            !isset($data['common_price'])
-        ) {
+        if (!$mailer->validate($data)) {
             throw new BadRequestHttpException();
         }
 
-        $default = [
-            'email' => '',
-            'common_price' => '',
-            'date' => '',
-            'type' => '',
-            'type_other' => '',
-            'audience' => '',
-            'aim' => '',
-            'time' => '',
-            'deadline' => '',
-            'example_1' => '',
-            'example_2' => '',
-            'example_3' => '',
-            'idea' => [],
-            'scenario' => [],
-            'heroes' => '',
-            'heroes_prof' => '',
-            'heroes_not_prof' => '',
-            'hero_search' => '',
-            'visagiste' => '',
-            'stylist' => '',
-            'storyboard' => '',
-            'days' => '',
-            'equipment[]' => [],
-            'equipment_other' => '',
-            'team' => '',
-            'montage' => '',
-            'color' => '',
-            'sound' => '',
-            'music' => '',
-            'music_other' => '',
-            'speaker' => '',
-            'speaker_lg' => [],
-            'copywriter' => '',
-            'animation' => '',
-            'animation_other' => '',
-            'branding' => '',
-            'branding_other' => '',
-            'social' => [],
-            'social_other' => '',
-        ];
-
-        foreach ($default as $k => $v) {
-            if (!isset($data[$k])) {
-                $data[$k] = $v;
-            }
-        }
-
-        $result = $this->sendEmail(
-            $mailer,
-            $settings,
-            $data['email'],
-            'brief',
-            $data
-        );
-
         return new JsonResponse([
-            'result' => $result ? 'ok' : 'error'
+            'result' => $mailer->send($data['email'], $data)
+                ? 'ok'
+                : 'error'
         ]);
-    }
-
-    protected function sendEmail(
-        \Swift_Mailer $mailer,
-        ContentService $settings,
-        $recipient,
-        $template,
-        $data = []
-    ) {
-        $to = [$recipient];
-        foreach (explode(',', $settings->get('contacts.email.'.$template)) as $email) {
-            $to[] = trim($email);
-        }
-
-        $message = (new \Swift_Message('Hello Email'))
-            ->setFrom($settings->get('contacts.email.from'))
-            ->setTo($to)
-            ->setBody(
-                $this->renderView(
-                    'email/'.$template.'.txt.twig',
-                    $data
-                ),
-                'text/plain'
-            );
-
-        return $mailer->send($message);
     }
 }
---
+++ src/AppBundle/Service/MailerService.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace AppBundle\Service;
+
+use Symfony\Component\Templating\EngineInterface;
+use Symfony\Component\Translation\Translator;
+
+class MailerService {
+
+    /** @var \Swift_Mailer */
+    private $mailer;
+
+    /** @var ContentService */
+    private $settings;
+
+    /** @var EngineInterface */
+    private $templating;
+
+    /** @var Translator */
+    private $translator;
+
+    /** @var string[] */
+    public $required = [];
+
+    /** @var array */
+    public $fields = [];
+
+    /** @var string */
+    public $subject;
+
+    /** @var string */
+    public $recipient;
+
+    /** @var string */
+    public $sender;
+
+    /** @var string */
+    public $template;
+
+    /** @var string */
+    public $contentType;
+
+    /**
+     * MailerService constructor.
+     * @param \Swift_Mailer $mailer
+     * @param ContentService $settings
+     * @param EngineInterface $templating
+     * @param Translator $translator
+     */
+    public function __construct(
+        \Swift_Mailer $mailer,
+        ContentService $settings,
+        EngineInterface $templating,
+        Translator $translator
+    ) {
+        $this->mailer = $mailer;
+        $this->settings = $settings;
+        $this->templating = $templating;
+        $this->translator = $translator;
+    }
+
+    /**
+     * @return \Swift_Mailer
+     */
+    public function getMailer()
+    {
+        return $this->mailer;
+    }
+
+    /**
+     * @param \Swift_Mailer $mailer
+     */
+    public function setMailer($mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * @return ContentService
+     */
+    public function getSettings()
+    {
+        return $this->settings;
+    }
+
+    /**
+     * @param ContentService $settings
+     */
+    public function setSettings($settings)
+    {
+        $this->settings = $settings;
+    }
+
+    /**
+     * @return EngineInterface
+     */
+    public function getTemplating()
+    {
+        return $this->templating;
+    }
+
+    /**
+     * @param EngineInterface $templating
+     */
+    public function setTemplating($templating)
+    {
+        $this->templating = $templating;
+    }
+
+    /**
+     * @param array $data
+     * @return bool
+     */
+    public function validate($data = []) {
+        foreach ($this->required as $field) {
+            if (!isset($data[$field])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @param array $data
+     * @return \Swift_Message
+     */
+    public function create($recipient, $data = []) {
+        return (new \Swift_Message())
+            ->setSubject($this->getSubject())
+            ->setTo($this->getTo($recipient))
+            ->setFrom($this->getFrom())
+            ->setBody(
+                $this->getBody($data),
+                $this->getContentType()
+            );
+    }
+
+    /**
+     * @param array $data
+     * @return bool|int
+     */
+    public function send($recipient, $data = []) {
+        if (!$this->validate($data)) {
+            return false;
+        }
+
+        return $this->mailer->send(
+            $this->create($recipient, $data)
+        );
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSubject() {
+        return trim(
+            $this->translator->trans(
+                $this->subject
+            )
+        );
+    }
+
+    /**
+     * @return string
+     */
+    protected function getFrom() {
+        return trim(
+            $this->settings->get(
+                $this->sender
+            )
+        );
+    }
+
+    /**
+     * @param string|string[] $recipient
+     * @return array
+     */
+    protected function getTo($recipient) {
+        $result = [];
+
+        if ($recipient) {
+            if (is_string($recipient)) {
+                $result = explode(',', $recipient);
+            } elseif (is_array($recipient)) {
+                $result = $recipient;
+            }
+        }
+
+        $admins = $this->settings->get(
+            $this->recipient
+        );
+
+        $result = array_merge(
+            $result,
+            explode(',', $admins)
+        );
+
+        foreach ($result as &$email) {
+            $email = trim($email);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    protected function getBody($data = []) {
+        return trim(
+            $this->templating->render(
+                $this->template,
+                $this->prepareData($data)
+            )
+        );
+    }
+
+    /**
+     * @return string
+     */
+    protected function getContentType() {
+        return $this->contentType;
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    protected function prepareData($data = []) {
+        $result = [];
+
+        foreach ($this->fields as $field => $default) {
+            $result[$field] = isset($data[$field]) && (gettype($data[$field]) == gettype($default))
+                ? $data[$field]
+                : $default;
+        }
+
+        return $result;
+    }
+
+}
